9e212e139243a7e588ecaa77cc288ca2a13886bf
fazle01 <h-m-fazle.rabbi@vitrox.com>
2019-10-14 20:41:28 +0800

    [Update]: Create update and delete post completed

--- DjangoBlog/django_project/blog/urls.py
+++ DjangoBlog/django_project/blog/urls.py
@@ -7,8 +7,13 @@
 """
 from django.urls import path
 from . import views
+from .views import PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView
 
 urlpatterns = [
-    path('', views.home, name='blog-home'),
+    path('', PostListView.as_view(), name='blog-home'),
+    path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'), #pk is what it expects it to be
+    path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'), #pk is what it expects it to be
+    path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'), #pk is what it expects it to be
+    path('post/new/', PostCreateView.as_view(), name='post-create'), #pk is what it expects it to be
     path('about/', views.about, name='blog-about'),
 ]
--- DjangoBlog/django_project/blog/views.py
+++ DjangoBlog/django_project/blog/views.py
@@ -1,21 +1,15 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+# from django.http import HttpResponse
 from .models import Post
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.views.generic import (
+	ListView, 
+	DetailView, 
+	CreateView,
+	UpdateView,
+	DeleteView
+	)
 
-# posts =[
-# 	{
-# 		'author': 'Fazle',
-# 		'title': 'Django unchained',
-# 		'content': 'Django was developed by the Django software foundation in the year 2005 and designed by Adrian Holovaty and Simon Willison.  The main feature of Django is the High-level python web framework as this framework provides the best way to design the application. As a developer needs to follow the rules or way to implement the project in Django, there is comparatively less freedom compared to the low-level framework but the application created will be neat and clean.',
-# 		'date_created': '10 October 2019'
-# 	},
-# 	{
-# 		'author': 'Tao',
-# 		'title': 'Oracle unchained',
-# 		'content': 'Oracle JDK was developed by Oracle Corporation which was under Sun License and was implemented based on the Java Standard Edition Specification.  It was completely based on Java programming language. Later the license was announced to be released under GPL (General Public License) License. Oracle JDK contains many components as a collection of programming tools in the form of a library.',
-# 		'date_created': '10 October 2019'
-# 	}
-# ]
 # Create your views here.
 def home(request):
 	context ={
@@ -23,6 +17,52 @@ def home(request):
 	}
 	return render(request, 'blog/home.html', context)
 
+class PostListView(ListView):
+	"""docstring for PostListView"""
+	model=Post
+	template_name='blog/home.html' #app/model_List.gtml
+	context_object_name='posts'
+	ordering=['-datecreated'] #- ve sign for reverse order
+
+class PostDetailView(DetailView):
+	"""docstring for PostListView"""
+	model=Post
+
+class PostCreateView(LoginRequiredMixin, CreateView):
+	"""docstring for PostListView"""
+	model=Post
+	fields=['title', 'content']
+
+	def form_valid(self, form):
+		form.instance.author=self.request.user
+		return super().form_valid(form)
+
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+	"""docstring for PostListView"""
+	model=Post
+	fields=['title', 'content']
+
+	def form_valid(self, form):
+		form.instance.author=self.request.user
+		return super().form_valid(form)
+
+	def test_func(self):
+		post=self.get_object()
+		if self.request.user==post.author:
+			return True
+		return False
+
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+	"""docstring for PostListView"""
+	model=Post
+	success_url='/'
+
+
+	def test_func(self):
+		post=self.get_object()
+		if self.request.user==post.author:
+			return True
+		return False
 
 def about(request):
 	return render(request, 'blog/about.html', {'title': 'Django Blog About'})
