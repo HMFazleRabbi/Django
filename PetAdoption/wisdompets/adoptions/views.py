b1da9a200b01b3dc61bb96254064a3ff64f3fbf7
fazle01 <h-m-fazle.rabbi@vitrox.com>
2020-06-20 17:32:24 +0800

    [Update]: Replaced the html with pages. Also seperated the css and the javascripts.

--- PetAdoption/wisdompets/adoptions/views.py
+++ PetAdoption/wisdompets/adoptions/views.py
@@ -1,9 +1,22 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import Http404
+
+
+from .models import Pet
 
 # Create your views here.
 def home(request):
-  return HttpResponse('<p> Home View</p>')
+    
+  pets = Pet.objects.all()
+  return render (request, "home.html", {
+      'pets': pets,
+  })
 
 def pet_detail(request, pet_id):
-    return HttpResponse(f"<p> Pet Details with id {pet_id}</p>")
+    try:
+        pet = Pet.objects.get(id=pet_id)
+    except Pet.DoesNotExist:
+        raise HttpResponse("Pet not found!")
+    return render (request, "pet_detail.html", {
+      'pet': pet,
+    })
